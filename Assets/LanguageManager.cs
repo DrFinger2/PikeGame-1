1963a1e8c318c296281543d5a21066fd3e1f0e5b
DoctorFinger <144325868+DoctorFinger@users.noreply.github.com>
2026-04-20 01:36:53 +0300

    accidentally broke some shit

--- Assets/LanguageManager.cs
+++ Assets/LanguageManager.cs
@@ -39,9 +39,9 @@ public class LanguageManager : MonoBehaviour
         transform.SetParent(null);  // Force the object to the root of the hierarchy so DontDestroyOnLoad works
         DontDestroyOnLoad(gameObject);
     }
-    
-    
-    
+
+
+
     public void SwitchLanguage(string language)
     {
         switch (language)
@@ -58,7 +58,15 @@ public class LanguageManager : MonoBehaviour
         }
         onLanguageChanged?.Invoke();
     }
+
+    public void SwitchLanguage(Language language)
+    {
+
+        currentLanguage = language;
+        onLanguageChanged?.Invoke();
+    }
 }
+
 public enum Language
 {
     FI,
